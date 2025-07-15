671010a66a2f9aea64da7b3f437b5a6734fc5abf
leck77 <a.oshatjuk@gmail.com>
2025-07-15 13:04:03 +0200

    add rename.mjs

---
+++ rename.mjs
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const directoryPath = path.join(__dirname, 'public', 'aaa');
+
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+
+  let i = 1;
+  files.forEach((file) => {
+    if (file.includes('.sor')) {
+      const oldPath = path.join(directoryPath, file);
+      const newPath = path.join(directoryPath, `REM${i}.sor`);
+      fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+          console.log('Error renaming file: ' + err);
+        } else {
+          console.log(`Renamed ${file} to REM${i}.sor`);
+        }
+      });
+      i++;
+    }
+  });
+});
